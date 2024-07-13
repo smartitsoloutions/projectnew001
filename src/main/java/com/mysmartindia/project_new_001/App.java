801e413a31b0abb3f7af9c8db8a5721186935399
smartitsoloutions <prasads128312@gmail.com>
2024-07-13 15:35:00 +0530

    this is my first new project 001

---
+++ src/main/java/com/mysmartindia/project_new_001/App.java
@@ -0,0 +1,13 @@
+package com.mysmartindia.project_new_001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
